0e047dfd6681cbebb98e3177e688a7bae02c5f01
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-04-04 23:01:42 -0400

    Finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, num) {
+    const args = Array.from(arguments);
+                    args.shift()
+        for (let i = 0; i < args.length; i++) {
+            const index = array.indexOf(args[i]);
+                if (index != -1) {
+                    array.splice(index, 1);
+                }
+        }
+    
+        return array
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
