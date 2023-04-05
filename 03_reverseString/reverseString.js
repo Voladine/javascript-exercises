3df44b8f64a57de0ea7e157b94fb6d3cbf9e74c5
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-04-04 21:55:18 -0400

    Finish 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    const array = string.split("")
+    const reverseArray = array.reverse()
+    const joinString = reverseArray.join("")
+    
+    return joinString
+    };
 
 // Do not edit below this line
 module.exports = reverseString;
