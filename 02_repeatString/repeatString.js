30521a0ed7a11d5bdd24b579afb867b36f72a9c4
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-04-04 21:33:16 -0400

    Finish 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = ""
+    if (num > 0) {
+        for (let i = 1; i <= num; i++) {
+        result += str
+        }
+    }
+    else if (num < 0) {
+        result = 'ERROR'
+        }
+        
+    return result
 };
 
 // Do not edit below this line
