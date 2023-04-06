3fc36c8d189662c20aeaf9d84353c7ea6f8f4e38
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-04-05 22:12:17 -0400

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    let finalSum = 0
+    if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR"
+    }
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            finalSum += i
+        }
+    }
+    else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            finalSum += i
+        }
+    }
+    return finalSum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
