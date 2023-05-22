4a704b4b53d403f48a822c420a95a379b297bba7
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-05-21 22:06:58 -0400

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const str = words
+        .toLowerCase()
+        .replaceAll(' ', '')
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .replace(/\s{2,}/g, " ")
+    let arr = []
+        for (let i = 0; i < str.length; i++) {
+            arr[i] = str[i]
+            }
+    let arrReverse = [...arr].reverse() 
 
+    let strReverse = ""
+        for (let i = 0; i < arrReverse.length; i++) {
+            strReverse += arrReverse[i]
+            }
+
+    if (str === strReverse) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
