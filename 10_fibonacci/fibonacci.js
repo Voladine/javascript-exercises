4224e0e508efda854b0444ee240cfa08c1dc218b
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-05-21 23:22:16 -0400

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
+    let arr = [1, 1]
+        for (let i = 0; i < (num - 2); i++) {
+            arr.push((arr[i]) + (arr[i + 1]))
+        }
+    return arr[num - 1]
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
